241fc6a57ec12167561a7590d47bf0bd4ad27546
PADFSG <noreply@padfsg.org>
2026-07-21 10:44:48 +0300

    Use new Google Sheets web app deployment

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Production submission destinations.
 window.PADFSG_FORM_CONFIG = Object.freeze({
   formspreeUrl: 'https://formspree.io/f/mgogyqza',
-  googleSheetsUrl: 'https://script.google.com/macros/s/AKfycbyeeD3uJMFdQ70QINRAUwZWdtvavxrivPQdO0Cdc0y_pR4MkS0_OfFAlHGaJmyW0_XN/exec'
+  googleSheetsUrl: 'https://script.google.com/macros/s/AKfycbwcD0fPCZxcOyfKNGXJqyC68IgMGcy_HxliehihZOlJmuJb_60nK-mWfhUf_6H2C4nV/exec'
 });
